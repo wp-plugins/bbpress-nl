6cc1c430af2daa47b7a4ce80e8c30adc0ca8d312
DeFries <DeFries@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-13 16:34:33 +0000

    Into the light I command thee, trunk version
    git-svn-id: https://plugins.svn.wordpress.org/bbpress-nl/trunk@857317 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+* This plugin will provide the Dutch translation for your bbPress installation
+*
+* @author Remkus de Vries
+*
+* Plugin Name: bbPress NL
+* Plugin URI: http://remkusdevries.com/plugins/bbpress-nl/
+* Description: This plugin will translate bbPress in Dutch.
+* Author: Remkus de Vries
+* Version: 2.5.0
+* Author URI: http://remkusdevries.com/
+* License: GPLv2
+* Copyright (C) 2014 Remkus de Vries, and each individual languages' contributors from http://translate.wordpress.org/projects/bbpress
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action( 'bbp_loaded', 'fsm_bbpress_do_nl_translation' );
+/**
+ * Change translations domain location
+ * @return alterd text_domain
+ */
+function fsm_bbpress_do_nl_translation() {
+
+	// load the location
+	$mofile = WP_PLUGIN_DIR . "/bbpress-nl/nederlands/" . 'bbpress-nl_NL.mo';
+
+	// inject it if it exists for bbPress
+	if ( file_exists( $mofile ) ) {
+		return load_textdomain( 'bbpress', $mofile );
+	}
+
+}
